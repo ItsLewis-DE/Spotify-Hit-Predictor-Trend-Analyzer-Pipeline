2f5f96b2e0e4ab422e2ab47cf4ac814494ab046c
ItsLewis-DE <thanhphong1110n@gmail.com>
2026-05-29 05:08:13 +0200

    fix_audio_feature

--- dags/scripts/crawl_audio_feature.py
+++ dags/scripts/crawl_audio_feature.py
@@ -45,21 +45,23 @@ def get_api_audio_feature(spotify_id_string: str,timezone: int)-> pd.DataFrame:
     BASE_URL = f'https://spotify-extended-audio-features-api.p.rapidapi.com/v1/audio-features'
     params = {'ids': spotify_id_string}
     if timezone ==1:
-        X-RapidAPI-Key = os.getenv('X-RapidAPI-Key-1')
+        X_RapidAPI_Key = os.getenv('X_RapidAPI_Key_1')
     elif timezone==2:
-        X-RapidAPI-Key = os.getenv('X-RapidAPI-Key-2')
+        X_RapidAPI_Key = os.getenv('X_RapidAPI_Key_2')
     elif timezone==3:
-        X-RapidAPI-Key = os.getenv('X-RapidAPI-Key-3')
+        X_RapidAPI_Key = os.getenv('X_RapidAPI_Key_3')
     else:
-        X-RapidAPI-Key = os.getenv('X-RapidAPI-Key-4')
+        X_RapidAPI_Key = os.getenv('X_RapidAPI_Key_4')
     headers = {
-        "X-RapidAPI-Key": X-RapidAPI-Key, 
+        "X-RapidAPI-Key": X_RapidAPI_Key, 
         "X-RapidAPI-Host": "spotify-extended-audio-features-api.p.rapidapi.com" 
     }
 
-    # Gọi API trực tiếp không cần biến params nữa
+    logger.info(f"API KEY : {X_RapidAPI_Key}")
     response = requests.get(BASE_URL, headers=headers,params = params)
-    response.raise_for_status()
+    if response.status_code == 429:
+        logger.warning("RATE LIMIT !!!")
+        return None
     if response.status_code == 200:
         logger.info("Extracting data audio feature....")
         df = pd.DataFrame(response.json()['audio_features'])
@@ -71,19 +73,28 @@ def get_audio_feature(input_file: Path,output_dir: Path):
     df_rank.rename(columns= {'uri':'spotify_id'},inplace=True)
     spotify_id_string = df_rank['spotify_id'].to_list()
     list_df = []
-    for timezone in [1,2,3,4]:
-        for i in range(0,50,5):
-            id_string = ','.join(spotify_id_string[i : i +5])
-            df = get_api_audio_feature(id_string,timezone)
-            if df is not None:
-                list_df.append(df)
-            time.sleep(1)
-    df_audio_feature = pd.concat(list_df,ignore_index =True)
+    timezone = [1,2,3,4]
+    id_tz=0
+    for i in range(0,len(spotify_id_string),5):
+        id_string = ','.join(spotify_id_string[i : i +5])
+        df = get_api_audio_feature(id_string,id_tz)
+        if df is not None:
+            list_df.append(df)
+        elif df is None:
+            id_tz+=1
+        if id_tz >= len(timezone):
+            break 
+        time.sleep(2)   
+    if list_df:
+        df_audio_feature = pd.concat(list_df,ignore_index =True)
+    else:
+        logger.error("There is no data..")
+        return
     df_merge = pd.merge(df_rank,df_audio_feature,left_on = 'spotify_id',right_on = 'id')
     date = '-'.join(input_file.stem.split('-')[3:])
     df_merge['fetched_at'] = pd.to_datetime(date)
     output_dir.mkdir(parents=True,exist_ok = True)
-    df_merge.to_json(f'{output_dir}/feature-{date}.json',orient='records',lines=True,force_ascii=False)
+    df_merge.to_json(f'{output_dir}/feature-{date}.json',orient='records',lines=True,force_ascii=False,date_format='iso')
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
